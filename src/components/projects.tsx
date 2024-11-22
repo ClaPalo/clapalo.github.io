99608465205114138a722e1a679bcdedff211e9f
Claudio Paloscia <claudioclaudio91@gmail.com>
2024-11-22 17:20:23 -0600

    Updated project

--- src/components/projects.tsx
+++ src/components/projects.tsx
@@ -1,12 +1,13 @@
 import "../project.css";
 import { WobbleCard } from "./ui/wobble-card";
 import { FaGithub } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
 
 function Projects() {
     return (
         <div
             id="projects"
-            className="w-full h-full flex flex-col justify-start items-center px-10 lg:px-10"
+            className="w-full h-fit flex flex-col justify-start items-center px-10 lg:px-10"
         >
             <h1 className="text-ternary font-bold text-4xl md:text-7xl">
                 My Projects
@@ -63,31 +64,39 @@ function Projects() {
                     containerClassName="col-span-2 rounded-2xl h-full relative card-cyan"
                     className="card card-cyan"
                 >
-                    <div className="card-content w-full h-full text-black">
+                    <div className="card-content w-full h-full text-white">
                         <div>
                             <h1 className="font-semibold text-4xl z-30">
-                                Academic Portfolio
+                                Inventory Management
                             </h1>
                             <p className="my-3 w-2/3 z-30">
-                                <strong>Responsive</strong> design of a
-                                professional portfolio, based on extensive use
-                                of <strong>React waypoints</strong> and{" "}
-                                <strong>CSS scroll snap</strong>.{" "}
+                                Demo of an inventory management Web Application fully deployed on AWS.
+                                Created using <strong>NextJS</strong>, <strong>ExpressJS</strong>, <strong>PostgreSQL</strong> and <strong>Prisma</strong>.<br />
+                                Deployed with <strong>Amplify</strong>, <strong>EC2</strong>, <strong>RDS</strong>, <strong>S3</strong> and <strong>API Gateway</strong>.
                             </p>
-                            <a
-                                href="https://github.com/ClaPalo/Academic-Portfolio"
-                                target="_blank"
-                                className="text-black hover:text-black z-30"
-                            >
-                                <FaGithub size={30} />
-                            </a>
+                            <div className="flex flex-row gap-3">
+                                <a
+                                    href="https://github.com/ClaPalo/inventory-management"
+                                    target="_blank"
+                                    className="text-white z-30"
+                                >
+                                    <FaGithub size={30} />
+                                </a>
+                                <a
+                                    href="https://main.d470kvzhkas7w.amplifyapp.com/"
+                                    target="_blank"
+                                    className="text-white z-30"
+                                >
+                                    <TbWorldWww size={30} />
+                                </a>
+                            </div>
                         </div>
                     </div>
                     <img
-                        src="/portfolio.png"
-                        alt="portfolio"
+                        src="/inventory.png"
+                        alt="inventory"
                         width={1000}
-                        className="absolute -bottom-[40%] -right-[40%] md:-bottom-[10%] lg:-bottom-[10%] xl:-bottom-[20%] 2xl:-bottom-[75%]  object-cover rounded-2xl scale-125"
+                        className="absolute -bottom-[40%] -right-[40%] md:-bottom-[10%] lg:-bottom-[10%] xl:-bottom-[70%] 2xl:-bottom-[160%]  object-cover rounded-2xl scale-125 blur-[2px]"
                     />
                 </WobbleCard>
                 <WobbleCard
@@ -113,8 +122,8 @@ function Projects() {
                     </div>
                 </WobbleCard>
             </div>
-            <div className="md:hidden grid grid-cols-1 w-5/6 h-full gap-3 mt-10">
-                <div className="col-span-1 card card-violet rounded-2xl h-full card-violet p-2 items-center">
+            <div className="md:hidden grid grid-cols-1 w-5/6 h-fit gap-3 mt-10">
+                <div className="col-span-1 card card-violet rounded-2xl h-full card-violet p-5 items-center">
                     <div className="card-content text-white flex flex-row justify-around">
                         <div className="flex flex-col justify-center items-center">
                             <a
@@ -152,25 +161,33 @@ function Projects() {
                         </div>
                     </div>
                 </div>
-                <div className="card card-cyancol-span-1 rounded-2xl h-full relative card-cyan p-2 items-center">
+                <div className="card card-cyan col-span-1 rounded-2xl h-full relative card-cyan p-2 items-center">
                     <div className="card-content w-full h-full text-white items-center flex flex-col justify-center">
                         <div className="flex flex-col justify-center items-center">
-                            <h1 className="font-semibold text-2xl z-30">
-                                Academic Portfolio
+                            <h1 className="font-semibold text-2xl z-30 text-center">
+                                Inventory Management
                             </h1>
                             <p className="my-3 w-2/3 z-30 text-center text-sm">
-                                <strong>Responsive</strong> design of a
-                                professional portfolio, based on extensive use
-                                of <strong>React waypoints</strong> and{" "}
-                                <strong>CSS scroll snap</strong>.{" "}
+                                Demo of an inventory management Web Application fully deployed on AWS.
+                                Created using <strong>NextJS</strong>, <strong>ExpressJS</strong>, <strong>PostgreSQL</strong> and <strong>Prisma</strong>.<br />
+                                Deployed with <strong>Amplify</strong>, <strong>EC2</strong>, <strong>RDS</strong>, <strong>S3</strong> and <strong>API Gateway</strong>.
                             </p>
-                            <a
-                                href="https://github.com/ClaPalo/Academic-Portfolio"
-                                target="_blank"
-                                className="text-white invalid:z-30"
-                            >
-                                <FaGithub size={25} />
-                            </a>
+                            <div className="flex flex-row gap-3">
+                                <a
+                                    href="https://github.com/ClaPalo/inventory-management"
+                                    target="_blank"
+                                    className="text-white z-30"
+                                >
+                                    <FaGithub size={25} />
+                                </a>
+                                <a
+                                    href="https://main.d470kvzhkas7w.amplifyapp.com/"
+                                    target="_blank"
+                                    className="text-white z-30"
+                                >
+                                    <TbWorldWww size={25} />
+                                </a>
+                            </div>
                         </div>
                     </div>
                 </div>
